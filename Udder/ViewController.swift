35031b24e11e66550ac365eae9614760e336bd8e
Sasha <sashaglonti@gmail.com>
2020-08-05 00:37:29 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Sasha on 05.08.2020.
+//  Copyright © 2020 Sasha Glonti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
